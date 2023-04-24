f5c18c79b351e46aa68ac2a303a19c92eb847002
pradojeric <jericprado0719@gmail.com>
2023-04-24 16:34:08 +0800

    hello

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -9,7 +9,38 @@
 
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
-                Roles Here
+                <div class="flex justify-end mb-2">
+                    <a href="{{ route('roles.create') }}">
+                        <x-button>Create</x-button>
+                    </a>
+                </div>
+                <table class="table-auto min-w-full">
+                    <thead>
+                        <tr class="bg-gray-300">
+                            <th class="tracking-tight px-3 py-2 uppercase">Name</th>
+                            <th class="tracking-tight px-3 py-2 uppercase">
+                                <span class="sr-only">
+                                    Action
+                                </span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($roles as $role)
+                            <tr>
+                                <td class="px-3 py-2 text-center">{{ $role->name }}</td>
+                                <td class="px-3 py-2 text-center">
+                                    <a href="{{ route('roles.show', $role) }}">
+                                        <x-button>Show</x-button>
+                                    </a>
+                                    <a href="{{ route('roles.edit', $role) }}">
+                                        <x-button>Edit</x-button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
 
