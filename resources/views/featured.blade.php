8908bb7a6f1d72a7f171e655086f78031cb0ec68
Hogunz <cjdc1414@gmail.com>
2023-08-09 16:52:40 +0800

    8/9/2023

--- resources/views/featured.blade.php
+++ resources/views/featured.blade.php
@@ -64,12 +64,11 @@
         </div>
     </section> --}}
 
-    <section class="lg:container lg:mx-auto p-8 w-full">
+    <section class="lg:container lg:mx-auto w-full">
         <div class="grid md:grid-cols-4 justify-items-center grid-cols-2 gap-4 p-2 ">
             @foreach ($gigs->shuffle()->take(8) as $gig)
                 <div class="w-full max-w-sm bg-white flex flex-col">
                     <!-- Added flex flex-col -->
-
                     <a href="{{ route('show.supplier-gig', ['user' => $gig->user, 'gig' => $gig]) }}">
                         <img class="rounded-lg flex-grow-0 flex-shrink-0"
                             src="{{ asset('storage/' . $gig->gigUploads->where('type', 'image')->first()->url) }}"
